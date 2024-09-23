d7875f2749653c8a1f5d089450a5704dd9b1fa60
Takudzwamz <zhout1709@hotmail.com>
2024-09-23 17:19:01 +0200

    Added responsivenes to the toogle navbar button on mobile phones

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { DarkModeService } from '../services/dark-mode.service';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgbCollapseModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-darkModeService: DarkModeService =inject(DarkModeService);
 
-toggleDarkMode(){
-  this.darkModeService.updateDarkMode();
-}
+  // Step 1:
+	// Create a property to track whether the menu is open.
+	// Start with the menu collapsed so that it does not
+	// appear initially when the page loads on a small screen!
+	isMenuCollapsed = true;
+  darkModeService: DarkModeService =inject(DarkModeService);
+
+  toggleDarkMode(){
+    this.darkModeService.updateDarkMode();
+  }
 }
